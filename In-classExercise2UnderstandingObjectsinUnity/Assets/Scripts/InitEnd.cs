52d3e0c3c4ade4d5d8c0b9ddcd99d4316a123b1a
Unknown <sam.lindsey@du.edu>
2019-04-08 16:33:16 -0600

    First example of rolling ball & Game objects
    Wording of second exercise was strange and I didn't really know what the end product was supposed to look like.... I tried (and failed).... I know I will have to learn this soon though as the first part is due next week. I will look to other people for help on Wednesday.

---
+++ In-classExercise2UnderstandingObjectsinUnity/Assets/Scripts/InitEnd.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InitEnd : MonoBehaviour
+{
+    public int initialPosition;
+    public int finalPosition;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //
+        //transform.Translate(initialPosition);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject obj = GameObject.FindGameObjectWithTag("SceneManager");
+        SceneManager sm = obj.GetComponent<SceneManager>();
+        
+        transform.Translate(initialPosition,0,0);
+        transform.Translate(finalPosition,0,0);
+        transform.Rotate(sm.rotationSpeed, 0,0);
+        //transform.localScale += (sm.scalingSpeed, 0,0 );
+            
+      
+        
+        
+    }
+}
---
+++ In-classExercise2UnderstandingObjectsinUnity/Assets/Scripts/SceneManagerVals.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+
+   public int movementSpeed = 1;
+   
+       public int rotationSpeed = 1;
+   
+       public int scalingSpeed = 1;
+    
+   
+}
