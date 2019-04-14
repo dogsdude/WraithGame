2eaed2bffe45911f1c85ec44be9864c71e7ace16
Unknown <sam.lindsey@du.edu>
2019-04-14 13:51:18 -0600

    First Commit of Game - Basic Layout
    Added sprites, player movement, camera direction, player speed, ship animation, camera boundaries, screen resolution

---
+++ Wraith/Assets/Scripts/ShipMovement/PlayShipMove.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using UnityEngine;
+
+
+
+public class PlayShipMove : MonoBehaviour
+{
+    private Animator animator;
+    private float maxAccel = 9f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 post = transform.position;
+    
+        //THIS MOVES PLAYER
+        //Returns float from -1 to 1, + = up, - = down
+        float y = Input.GetAxis("Vertical") ;
+        //Returns float from -1 to 1, + = right, - = left
+        float x = Input.GetAxis("Horizontal") * -1 ;
+        //Not using z axis
+      
+        Vector2 direction = new Vector2(x,y).normalized;
+        Movement(direction);
+
+    }
+
+    void Movement(Vector2 direction)
+    {
+        Vector2 min = Camera.main.ViewportToWorldPoint(new Vector3(0, 0));
+        Vector2 max = Camera.main.ViewportToWorldPoint(new Vector3(1, 1));
+
+        max.x -= 0.75f;
+        min.x += 0.75f;
+
+        max.y -= 1f;
+        min.y += 1f;
+
+        //Current position
+        Vector2 pos = transform.position;
+
+        //New position
+        pos += direction * maxAccel * Time.deltaTime;
+
+        //Sets boundaries for player
+        pos.x = Mathf.Clamp(pos.x, min.x, max.x);
+        pos.y = Mathf.Clamp(pos.y, min.y, max.y);
+
+        //New position
+        transform.position = pos;
+
+    }
+    
+    
+}
---
+++ Wraith/Assets/Scripts/ShipMovement/PlayerShipBounds.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boundary
+{
+    public float xMin, xMax, yMin, yMax;
+}
+
+
+public class PlayerShipBounds : MonoBehaviour
+{
+    public Boundary bounds;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+            transform.position= new Vector3
+            ( Mathf.Clamp( transform.position.x, bounds.xMin, bounds.xMax),
+                Mathf.Clamp( transform.position.y, bounds.yMin, bounds.yMax),
+                0.0f
+            );
+        
+
+       
+    }
+}
