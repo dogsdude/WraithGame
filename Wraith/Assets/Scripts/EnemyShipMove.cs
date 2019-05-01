6df25c5e23a5af70353e813a336182e860edcd88
Unknown <sam.lindsey@du.edu>
2019-04-30 18:58:11 -0600

    Added Audio
    Added all audio file assets, the files aren't attached to anything yet, but they exist

--- Wraith/Assets/Scripts/EnemyShipMove.cs
+++ Wraith/Assets/Scripts/EnemyShipMove.cs
@@ -6,11 +6,7 @@ public class EnemyShipMove : MonoBehaviour
 {
     //Speed of enemy ship
     public float speed; 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   
 
     // Update is called once per frame
     void Update()
